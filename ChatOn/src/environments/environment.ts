320aa97cea69caa07878d704ae2235a8149e7deb
jayeshs999 <55800453+jayeshs999@users.noreply.github.com>
2020-04-17 12:48:32 +0530

    Update environment.ts

--- ChatOn/src/environments/environment.ts
+++ ChatOn/src/environments/environment.ts
@@ -6,14 +6,7 @@ export const environment = {
   production: false,
   firebase:
   {
-    apiKey: "AIzaSyBMiXHQMdgdv_JuteR3MUPqbjhIzGtUB48",
-    authDomain: "chatdata-ad5b7.firebaseapp.com",
-    databaseURL: "https://chatdata-ad5b7.firebaseio.com",
-    projectId: "chatdata-ad5b7",
-    storageBucket: "chatdata-ad5b7.appspot.com",
-    messagingSenderId: "40915209682",
-    appId: "1:40915209682:web:25613b225bba186d024c0b",
-    measurementId: "G-52RSGP8XDZ"
+    //your firebase config goes here
   }
 };
 
